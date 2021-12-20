f2d5494adec397469f4c9aac646b71e5364cf31e
Skruphold <hunterco999@gmail.com>
2021-12-20 15:53:31 -0600

    added timer to html and css just added functions and got start quiz working

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
-var startButton = document.getElementsByClassName("start-quiz")
+const startButton = document.getElementById('start-butn')
+const quizBoxEl = document.getElementById('quiz-box')
+
+startButton.addEventListener('click', startQuiz)
 
 function startQuiz() {
-    console.log("started");
+    // console.log('started')
+    startButton.classList.add('hide')
+    quizBoxEl.classList.remove('hide')
+
+}
+
+function nextQuestion() {
+
+}
+
+function chooseAnswer() {
+
 }
