9301a98d484310ce6e0f4ad1aa10d072dc075bdf
Skruphold <hunterco999@gmail.com>
2021-12-20 16:30:22 -0600

    set interval for timer and made a lose game function

--- script.js
+++ script.js
@@ -1,13 +1,31 @@
-const startButton = document.getElementById('start-butn')
-const quizBoxEl = document.getElementById('quiz-box')
+const startButton = document.getElementById('start-butn');
+const quizBoxEl = document.getElementById('quiz-box');
+var timerEl = document.getElementById('timer');
 
-startButton.addEventListener('click', startQuiz)
+var timer;
+var timerCount;
+
+startButton.addEventListener('click', startQuiz);
 
 function startQuiz() {
     // console.log('started')
-    startButton.classList.add('hide')
-    quizBoxEl.classList.remove('hide')
+    timerCount = 30;
+    startButton.classList.add('hide');
+    quizBoxEl.classList.remove('hide');
+    startTimer();
+    nextQuestion();
 
+}
+
+function startTimer() {
+    timer = setInterval(function() {
+        timerCount--;
+        timerEl.textContent = timerCount;
+        if (timerCount === 0) {
+            clearInterval(timer);
+            loseGame();
+        }
+    }, 1000);
 }
 
 function nextQuestion() {
@@ -16,4 +34,9 @@ function nextQuestion() {
 
 function chooseAnswer() {
 
+}
+
+function loseGame() {
+    startButton.classList.remove('hide');
+    quizBoxEl.classList.add('hide');
 }
